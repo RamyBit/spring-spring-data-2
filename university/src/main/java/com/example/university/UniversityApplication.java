018c1d4bcac8df749fb6dc998bbdbabf1b3cd4b9
RamyBit <118202722+RamyBit@users.noreply.github.com>
2024-01-30 12:00:10 +0100

    Refactoring UniversityApplication

--- university/src/main/java/com/example/university/UniversityApplication.java
+++ university/src/main/java/com/example/university/UniversityApplication.java
@@ -1,6 +1,7 @@
 package com.example.university;
 
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -18,54 +19,9 @@ import java.util.Properties;
 /**
  * Configuration Class for the University library
  */
-@Configuration
-@EnableTransactionManagement
-@ComponentScan("com.example.university")
+@SpringBootApplication
 public class UniversityApplication {
+    public static void main(String[] args){
 
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource());
-        em.setPackagesToScan("com.example.university.domain");
-
-        final JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-        em.setJpaProperties(additionalProperties());
-
-        return em;
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-        //dataSource.setUsername(env.getProperty("jdbc.user"));
-        //dataSource.setPassword(env.getProperty("jdbc.pass"));
-
-        return dataSource;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        final JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return transactionManager;
-    }
-//
-//    @Bean
-//    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-//        return new PersistenceExceptionTranslationPostProcessor();
-//    }
-
-    final Properties additionalProperties() {
-        final Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
-        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        hibernateProperties.setProperty("hibernate.cache.use_second_level_cache", "false");
-
-        return hibernateProperties;
     }
 }
