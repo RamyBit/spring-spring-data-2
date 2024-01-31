eabb3f3b7d36131850fcd653a968818d60e25f0c
RamyBit <118202722+RamyBit@users.noreply.github.com>
2024-01-31 11:27:32 +0100

    Refactoring using @Query annotation

--- university/src/main/java/com/example/university/repo/StudentRepo.java
+++ university/src/main/java/com/example/university/repo/StudentRepo.java
@@ -2,8 +2,9 @@ package com.example.university.repo;
 
 import com.example.university.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,18 +15,23 @@ public interface StudentRepo extends JpaRepository<Student, Integer> {
 
     List<Student> findByAttendeeLastName(String lastName);
 
-    // findOldest
-    Optional<Student> findTopByOrderByAgeDesc();
+    // findOldest,  findTopByOrderByAgeDesc()
+    @Query(value = "SELECT * FROM Student s ORDER BY s.age DESC LIMIT 1", nativeQuery = true)
+    Optional<Student> findOldest();
 
-    List<Student> findByAttendeeFirstNameAndAttendeeLastName(String firstName, String lastName);
+    @Query("SELECT s FROM Student s WHERE s.attendee.firstName = :firstName and s.attendee.lastName = :lastName")
+    List<Student> findByFirstAndLastName(@Param("firstName") String firstName, @Param("lastName") String lastName);
 
     List<Student> findByAgeLessThan(int age);
 
-    List<Student> findByAttendeeLastNameLike(String nameCriteria);
+    @Query("SELECT s FROM Student s where s.attendee.lastName LIKE %:nameCriteria%")
+    List<Student> findBySimilarLastName(@Param("nameCriteria") String nameCriteria);
 
     //findFirstInAlphabet
-    Optional<Student> findFirstByOrderByAttendeeLastNameAsc();
+    @Query(value = "SELECT * FROM Student s ORDER BY s.last_name ASC LIMIT 1",nativeQuery = true)
+    Optional<Student> findFirstInAlphabet();
 
     // find3Oldest
-    List<Student> findTop3ByOrderByAgeDesc();
+    @Query(value = "SELECT TOP 3 * FROM Student ORDER BY age DESC", nativeQuery = true)
+    List<Student> find3Oldest();
 }
