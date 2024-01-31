83926a0ef13c286a701a8d06dd0026d08c329b46
RamyBit <118202722+RamyBit@users.noreply.github.com>
2024-01-31 14:56:19 +0100

    Specification Added

--- university/src/main/java/com/example/university/repo/CourseRepo.java
+++ university/src/main/java/com/example/university/repo/CourseRepo.java
@@ -2,12 +2,13 @@ package com.example.university.repo;
 
 import com.example.university.domain.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
 
-public interface CourseRepo extends JpaRepository<Course, Integer> {
+public interface CourseRepo extends JpaRepository<Course, Integer>, JpaSpecificationExecutor<Course> {
 
     Optional<Course> findByName(String name);
     List<Course> findByDepartmentChairMemberLastName(String chair);
