acf545d94c2d8a20ba7c57ffa3274eb6130ce358
RamyBit <118202722+RamyBit@users.noreply.github.com>
2024-02-01 10:48:51 +0100

    Application Class fixed

--- university/src/main/java/com/example/university/repo/StaffRepo.java
+++ university/src/main/java/com/example/university/repo/StaffRepo.java
@@ -2,9 +2,11 @@ package com.example.university.repo;
 
 import com.example.university.domain.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "staff", collectionResourceRel = "staff")
 public interface StaffRepo extends JpaRepository<Staff, Integer> {
     List<Staff> findByMemberLastName(String lastName);
 
